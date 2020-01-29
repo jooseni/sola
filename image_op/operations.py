c0f9ebfcc36242da13b7584d68c03d0a2ae2436c
engrliyah <jimohhaminatalliya@gmail.com>
2020-01-28 22:36:14 -0600

    hw0

--- image_op/operations.py
+++ image_op/operations.py
@@ -2,18 +2,25 @@ class operation:
 
     def blend(self, image, logo, x=None, y=None, alpha=None):
         '''
-        Use logo and blend it on the image at location (x, y)
-        image: the original image
-        logo: the logo to blend on to the image
-        x: topleft coordinate x
-        y: topleft coordinate y
-        alpha: blend ratio to the logo
+                Use logo and blend it on the image at location (x, y)
+                image: the original image
+                logo: the logo to blend on to the image
+                x: topleft coordinate x
+                y: topleft coordinate y
+                alpha: blend ratio to the logo
 
-        returns the image blended with logo at loction (x, y)
-        '''
-		#code
-        ### add your code here
-        ### Please do not change the structure
+                returns the image blended with logo at loction (x, y)
+                '''
+        y1o, y2o = max(0, -y), min(logo.shape[1], image.shape[1] - x)
+        x1o, x2o = max(0, -x), min(logo.shape[0], image.shape[0] - y)
+        y1, y2 = max(0, y), min(image.shape[1], x + logo.shape[0])
+        x1, x2 = max(0, x), min(image.shape[0], y + logo.shape[1])
 
-        return image #Currently the original image is returned, please replace this with the blended image
+        if y1 >= y2 or x1 >= x2 or y1o >= y2o or x1o >= x2o:
+            return
+
+        for c in range(3):
+            image[y1:y2, x1:x2] = (alpha * logo[y1o:y2o, x1o:x2o] + (1 - alpha) * image[y1:y2, x1:x2])
+
+            return image
 
