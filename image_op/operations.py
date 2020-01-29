50d646182ea3caf7ce1b502aa27c8b83d51bea77
pmantini <m_pranav@yahoo.com>
2020-01-28 22:15:55 -0600

    Initial commit

---
+++ image_op/operations.py
@@ -0,0 +1,19 @@
+class operation:
+
+    def blend(self, image, logo, x=None, y=None, alpha=None):
+        '''
+        Use logo and blend it on the image at location (x, y)
+        image: the original image
+        logo: the logo to blend on to the image
+        x: topleft coordinate x
+        y: topleft coordinate y
+        alpha: blend ratio to the logo
+
+        returns the image blended with logo at loction (x, y)
+        '''
+		#code
+        ### add your code here
+        ### Please do not change the structure
+
+        return image #Currently the original image is returned, please replace this with the blended image
+
